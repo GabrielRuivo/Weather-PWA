77ab2dab19483550488a3f501b62df28e6d8e636
Gabriel Ruivo <gabriel-ruivo@hotmail.com>
2020-10-05 10:40:11 -0300

    second commit

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { FiSearch } from 'react-icons/fi';
 import {Container, Header, Body, Footer} from './style';
